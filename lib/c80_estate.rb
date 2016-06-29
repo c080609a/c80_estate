0bdc51d3b4ac771ef4e8e23b9b98ddb3716edd2c
C80609A <c080609a@gmail.com>
2016-06-29 07:31:13 +0700

    master: initial commit

---
+++ lib/c80_estate.rb
@@ -0,0 +1,8 @@
+require "c80_estate/version"
+require "c80_estate/engine"
+
+module C80Estate
+  def self.table_name_prefix
+    'c80_estate_'
+  end
+end
---
+++ lib/c80_estate/version.rb
@@ -0,0 +1,3 @@
+module C80Estate
+  VERSION = "0.1.0"
+end
