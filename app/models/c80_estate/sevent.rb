b71ccb68bcf299bd608507b46872e1890ac85e9c
C80609A <c080609a@gmail.com>
2016-07-13 11:57:23 +0700

    develop: Sevent: добавил метод ecoef

--- app/models/c80_estate/sevent.rb
+++ app/models/c80_estate/sevent.rb
@@ -20,6 +20,86 @@ module C80Estate
     end
 =end
 
+    def self.ecoef(prop_id: nil, atype_id: nil, start_date: nil, end_date: nil)
+
+      # unless end_date.present?
+      #   end_date = Time.now.utc #.to_s(:db)
+      # end
+      # unless start_date.present?
+      #   if prop_id.present?
+      #     start_date =
+      #   end
+      # end
+
+      # произведём выборку из базы в list согласно параметрам
+      list = []
+
+      # если ничего не подано - выбираем всё и считаем среднее
+      if prop_id.nil? && atype_id.nil? && start_date.nil? && end_date.nil?
+
+        # хэш, где ключ - area_id
+        # а значение - объект вида {time_free, time_busy} -
+        # время, сколько площадь была в аренде или была свободна
+        result = {}
+
+        # раскидаем все sevents по area_id related спискам
+        self.all.each do |sevent|
+          aid = sevent.area_id
+          unless result[aid].present?
+            result[aid] = {
+                time_free: 0,
+                time_busy: 0,
+                ecoef:0,
+                sevents: []
+            }
+          end
+          result[aid][:sevents] << sevent
+        end
+
+        # теперь пробежимся по спискам и посчитаем время
+        result.each_key do |area_id|
+
+          # фиксируем area
+          a = result[area_id]
+
+          # переберём area related sevents
+          a.sevents.each_with_index do |sevent, index|
+
+            # если это первый элемент (т.е. до меня нет никого)
+            if index == 0
+
+            # если перед элементом есть кто-то
+            else
+
+              # фиксируем предыдущий элемент
+              prev_sevent = a.sevents[index-1]
+
+              # и считаем его длительность
+              d = sevent.created_at - prev_sevent.created_at
+
+              case prev_sevent.astatus.tag
+                when 'free'
+                  a[:time_free] += d
+                when 'busy'
+                  a[:time_busy] += d
+              end
+            end
+          end
+        end
+
+        # теперь имеется result в котором посчитаны time_free и time_busy
+        # каждой площади
+        # пробежимся по нему и посчитаем коэф-ты
+        result.each_key do |area_id|
+          a = result[area_id]
+          a[:ecoef] = a[:time_busy] / a[:time_busy] + a[:time_free]
+          Rails.logger.debug "<ecoef> area_id=#{area_id}, time_free=#{a[:time_free]}, time_busy=#{a[:time_busy]}, ecoef=#{a[:ecoef]}"
+        end
+
+      end
+
+    end
+
     def area_title
       res = "-"
       if area.present?
